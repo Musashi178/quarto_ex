3e76b73184486b49388204c700c3d1a3b52b9905
Marco Spatz <marco.spatz@googlemail.com>
2016-04-12 20:50:48 +0200

    removed game_state_test.exs

--- test/quarto/game_state_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule QuartoTest.GameStateTest do
-  use ExUnit.Case
-  doctest Quarto.GameStateTest
-
-  test "new returns a state with next player is one" do
-    assert GameState.new == {}
-  end
-end
