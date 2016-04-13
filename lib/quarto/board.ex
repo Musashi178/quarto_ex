6d599caa25c48a7ec3fdddc3a691ff24ada39351
Marco Spatz <marco.spatz@googlemail.com>
2016-04-13 17:38:44 +0200

    implemented win logic

--- lib/quarto/board.ex
+++ lib/quarto/board.ex
@@ -1,15 +1,78 @@
 defmodule Quarto.Board do
-  defstruct fields: for n <- 0..15, do: nil
+  defstruct fields: for _n <- 0..15, do: nil
 
   def is_empty?(%Quarto.Board{fields: fields}) do
     Enum.all?(fields, fn f -> f == nil end)
   end
 
-  def set_stone(%Quarto.Board{fields: fields}, row, column, %Quarto.Stone{}) do
+  def has_win_state?(board) do
+    win_sets = Enum.concat([Board.get_rows(board), Board.get_columns(board), Board.get_diagonals(board)])
+    Enum.any?(win_sets, is_win_line?) 
+  end
+
+  def is_win_line?(stone_line) do
+    cond do
+      has_empty_field?(stone_line) -> false
+      has_unique_attr?(stone_line) -> true
+      true -> false
+    end
+  end
+
+  def has_unique_attr?(stones) do
+    Enum.any?(get_unique_attrs(stones))
+  end
+
+  def get_unique_attrs(stones) do
+    %Quarto.Stone{}
+      |> Map.keys
+      |> Enum.filter(fn k -> k != :__struct__ end)
+      |> Enum.filter(fn attr -> is_unique_attr?(stones, attr) end)
+  end
+
+  def is_unique_attr?(stones, attr) do
+    num_different_attr = stones |> Enum.uniq_by(fn s -> Map.fetch!(s, attr) end) |> Enum.count
+    num_different_attr == 1
+  end
+
+  def has_empty_field?(fields) do
+    Enum.any?(fields, fn f -> f == nil end)
+  end
+
+  def get_rows(board) do
+    1..4 |> Enum.map(fn row_index -> Quarto.Board.get_rows(board, row_index) end)
+  end
+
+  def get_row(board, row_index) do
+    1..4 |> Enum.map(fn column_index -> Quarto.Board.get_stone(board, row_index, column_index) end)
+  end
+
+  def get_columns(board) do
+    1..4 |> Enum.map(fn column_index -> Quarto.Board.get_columns(board, column_index) end)
+  end
+
+  def get_column(board, column_index) do
+    1..4 |> Enum.map(fn row_index -> Quarto.Board.get_stone(board, row_index, column_index) end)
+  end
+
+  def get_diagonals(board) do
+    [:top_down, :bottom_up] |> Enum.map(fn d -> get_diagonal(board, d) end)
+  end
+
+  def get_diagonal(board, :top_down) do
+    1..4 |> Enum.map(fn index -> Quarto.Board.get_stone(board, index, index) end)
+  end
+
+  def get_diagonal(board, :bottom_up) do
+    1..4 |> Enum.map(fn index -> Quarto.Board.get_stone(board, 5 - index, index) end)
+  end
+
+  def set_stone(%Quarto.Board{fields: fields} = board, row, column, %Quarto.Stone{} = stone) do
+    index = compute_index(row, column)
+    %Quarto.Board{board | fields: List.replace_at(fields, index, stone)}
   end
 
   def get_stone(%Quarto.Board{fields: fields}, row, column) do
-
+    Enum.at(fields, compute_index(row, column))
   end
 
   @doc """
@@ -24,7 +87,14 @@ defmodule Quarto.Board do
 
         iex> Quarto.Board.compute_index(4, 4)
         15
+
+        iex> Quarto.Board.compute_index(5, 0)
+        ** (ArgumentError) row and column must be in range [1; 4]
     """
+  def compute_index(row, column) when row < 1 or row > 4 or column < 1 or column > 4 do
+      raise ArgumentError, message: "row and column must be in range [1; 4]"
+  end
+
   def compute_index(row, column) do
     ((row-1) * 4) + (column - 1)
   end
--- test/quarto_test/board_test.exs
+++ test/quarto_test/board_test.exs
@@ -4,8 +4,109 @@ defmodule QuartoTest.Board do
 
   alias Quarto.Board
 
+  # "setup_all" is called once to setup the case before any test is run
+ setup_all do
+   initial_board = %Board{}
+   full_board = %Board{fields: Quarto.Stone.get_all_stones()}
+
+   stone_a = %Quarto.Stone{size: :small, color: :white, shape: :round, top: :flat}
+   stone_b = %Quarto.Stone{ stone_a | size: :large}
+   stone_c = %Quarto.Stone{ stone_a | color: :black}
+   stone_d = %Quarto.Stone{ stone_a | shape: :square}
+   stone_e = %Quarto.Stone{ stone_a | shape: :square, top: :round}
+
+   win_line = [stone_a, stone_b, stone_c, stone_d]
+   non_win_line = [stone_a, stone_b, stone_c, stone_e]
+
+   # No metadata
+   {:ok, initial_board: initial_board, full_board: full_board, win_line: win_line, non_win_line: non_win_line }
+ end
+
+ setup do
+   random_stone = Quarto.Stone.get_all_stones() |> Enum.random
+   {:ok, random_stone: random_stone, random_column: 1, random_row: 2}
+ end
+
   test "all fields of an initial board are nil" do
     assert Board.is_empty?(%Board{})
   end
 
+  test "set_stone returns a non empty board", %{ initial_board: board, random_row: row, random_column: column, random_stone: stone } do
+    result = Board.set_stone(board, row, column, stone)
+    refute Board.is_empty?(result)
+  end
+
+  test "set_stone returns a board containing the set stone", %{ initial_board: board, random_row: row, random_column: column, random_stone: stone } do
+    result = Board.set_stone(board, row, column, stone)
+    assert Enum.member?(result.fields, stone)
+  end
+
+  test "get_stone returns nil on an empty field", %{ initial_board: board, random_row: row, random_column: column }  do
+    stone = Board.get_stone(board, row, column)
+    assert stone === nil
+  end
+
+  test "get_stone returns stone set", %{ initial_board: board, random_row: row, random_column: column, random_stone: stone} do
+    result = board
+              |> Board.set_stone(row, column, stone)
+              |> Board.get_stone(row, column)
+
+    assert result === stone
+  end
+
+  test "get_row with full_board returns 4 elements", %{ full_board: board, random_row: row} do
+    result_row = Board.get_row(board, row)
+    assert Enum.count(result_row)  === 4
+  end
+
+  test "get_column with full_board returns 4 elements", %{ full_board: board, random_column: column} do
+    result_column = Board.get_column(board, column)
+    assert Enum.count(result_column)  === 4
+  end
+
+  test "get_row and get_column with a full_board must have one common element", %{ full_board: board, random_row: row, random_column: column} do
+      result_row = Board.get_row(board, row)
+      result_column = Board.get_column(board, column)
+
+      stones_in_column_and_row = MapSet.intersection(MapSet.new(result_row), MapSet.new(result_column))
+
+      assert stones_in_column_and_row === MapSet.new([Board.get_stone(board, row, column)])
+  end
+
+  test "is_unique_attr? with unique attribute :top returns true", %{win_line: stones} do
+    assert Board.is_unique_attr?(stones, :top)
+  end
+
+  test "is_unique_attr? with non-unique attribute :color returns false", %{win_line: stones} do
+    refute Board.is_unique_attr?(stones, :color)
+  end
+
+  test "get_unique_attrs with win_line returns :top", %{win_line: stones} do
+    assert Board.get_unique_attrs(stones) == [:top]
+  end
+
+  test "get_unique_attrs with non_win_line returns empty list", %{non_win_line: stones} do
+    assert Board.get_unique_attrs(stones) == []
+  end
+
+  test "has_unique_attr? with win_line returns true", %{win_line: stones} do
+    assert Board.has_unique_attr?(stones)
+  end
+
+  test "has_unique_attr? with non_win_line returns false", %{non_win_line: stones} do
+    assert Board.has_unique_attr?(stones) === false
+  end
+
+  test "is_win_line? with win_line returns true", %{win_line: stones} do
+    assert Board.is_win_line?(stones)
+  end
+
+  test "is_win_line? with line were not all fields are set returns false", %{win_line: win_line} do
+    stones = List.replace_at(win_line, 1, nil)
+    assert Board.is_win_line?(stones) === false
+  end
+
+  test "is_win_line? with non_win_line returns false", %{non_win_line: stones} do
+    assert Board.is_win_line?(stones) === false
+  end
 end
