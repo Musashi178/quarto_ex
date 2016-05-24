c54e008b8bab9ca1261468a6feda61b179f8f18a
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-05-24 21:44:50 +0200

    added guardian serialiser

---
+++ apps/quarto_web/web/auth/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule QuartoWeb..GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias QuartoWeb.Repo
+  alias QuartoWeb.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
