396b85bb18bb982425b6b7f92b1fe7bd3b8cf32a
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-05-25 17:00:21 +0200

    added user auth JWT token (no PW check yet)

--- apps/quarto_web/web/controllers/session_controller.ex
+++ apps/quarto_web/web/controllers/session_controller.ex
@@ -2,19 +2,26 @@ defmodule QuartoWeb.SessionController do
   use QuartoWeb.Web, :controller
 
   alias QuartoWeb.User
-  alias QuartoWeb.Session
 
-  plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _params) do
     render conn, "new.html"
   end
 
   def create(conn, %{"session" => %{"email_or_username" => email_or_username, "password" => password}}) do
+    verified_user = Repo.get_by(User, email: email_or_username) || Repo.get_by(User, username: email_or_username)
+
+    IO.inspect (verified_user)
+
     conn
+    |> put_flash(:info, "Logged in.")
+    |> Guardian.Plug.sign_in(verified_user)
+    |> redirect(to: "/")
   end
 
   def delete(conn, _params) do
-    conn
+    Guardian.Plug.sign_out(conn)
+    |> put_flash(:info, "Logged out successfully.")
+    |> redirect(to: "/")
   end
 end
