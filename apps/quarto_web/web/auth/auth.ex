a1bb52c195d4ecea0f495efea62744ca17c5f668
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-05-29 10:23:43 +0200

    fixed auth plug

--- apps/quarto_web/web/auth/auth.ex
+++ apps/quarto_web/web/auth/auth.ex
@@ -2,13 +2,11 @@ defmodule QuartoWeb.Auth do
   import Plug.Conn
 
   def init(opts) do
-
   end
 
   def call(conn, repo) do
-    res = Guardian.Plug.current_resource(conn)
-    user = res && res.current_user
-    assign(conn, :current_user, user)
+    current_user = Guardian.Plug.current_resource(conn)
+    assign(conn, :current_user, current_user)
   end
 
 end
