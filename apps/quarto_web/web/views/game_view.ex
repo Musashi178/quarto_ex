8bcd3a6cd960f6c2b76306e9a2b93fc140d0ecf6
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-01 15:26:56 +0200

    adds opponent selection to new game form

--- apps/quarto_web/web/views/game_view.ex
+++ apps/quarto_web/web/views/game_view.ex
@@ -1,3 +1,15 @@
 defmodule QuartoWeb.GameView do
   use QuartoWeb.Web, :view
+
+  import Ecto.Query, only: [from: 2]
+
+  alias QuartoWeb.{User, Repo}
+
+  def get_available_opponents(%User{id: current_user_id}) do
+    query = from u in User,
+            where: u.id != ^current_user_id,
+            select: u.username
+
+    Repo.all(query)
+  end
 end
