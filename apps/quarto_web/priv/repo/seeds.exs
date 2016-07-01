44357a688bf69d7379af93526378db038f6890fd
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-01 18:57:19 +0200

    adds three users to the database seeds script

--- apps/quarto_web/priv/repo/seeds.exs
+++ apps/quarto_web/priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+pw = "password"
+QuartoWeb.Repo.insert!(QuartoWeb.User.registration_changeset(%QuartoWeb.User{}, %{ email: "player_one@quarto.com", username: "player_one", password: pw}))
+QuartoWeb.Repo.insert!(QuartoWeb.User.registration_changeset(%QuartoWeb.User{}, %{ email: "player_two@quarto.com", username: "player_two", password: pw}))
+QuartoWeb.Repo.insert!(QuartoWeb.User.registration_changeset(%QuartoWeb.User{}, %{ email: "player_three@quarto.com", username: "player_three", password: pw}))
