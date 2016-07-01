34908a645ed9ca014e6e357657ce489e69c8e6ed
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-02 01:19:16 +0200

    updates brunch and fixes some smaller issues

--- apps/quarto_web/web/models/user.ex
+++ apps/quarto_web/web/models/user.ex
@@ -12,14 +12,8 @@ defmodule QuartoWeb.User do
     timestamps
   end
 
-  @required_fields ~w(email username)
-  @optional_fields ~w()
-
   @doc """
   Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
   """
   def changeset(model, params \\ %{}) do
     model
