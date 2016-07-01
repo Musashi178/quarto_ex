a549586c135489473fdcdb24c3addb327a1d2e2a
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-02 01:07:04 +0200

    replaces obsolete echo calls in user model

--- apps/quarto_web/web/models/user.ex
+++ apps/quarto_web/web/models/user.ex
@@ -21,9 +21,10 @@ defmodule QuartoWeb.User do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, [:email, :username])
+    |> validate_required([:email, :username])
     |> validate_format(:email, ~r/@/)
     |> validate_length(:username, min: 3, max: 30)
     |> unique_constraint(:username)
@@ -33,7 +34,8 @@ defmodule QuartoWeb.User do
   def registration_changeset(model, params) do
     model
     |> changeset(params)
-    |> cast(params, ~w(password), [])
+    |> cast(params, [:password])
+    |> validate_required([:password])
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash
   end
