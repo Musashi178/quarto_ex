f73371a70592c78c4212d9f89a53c8cf5bc5d273
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-02 19:19:35 +0200

    updates game model to ecto2

--- apps/quarto_web/web/models/game.ex
+++ apps/quarto_web/web/models/game.ex
@@ -9,18 +9,16 @@ defmodule QuartoWeb.Game do
     timestamps
   end
 
-  @required_fields ~w(player_one player_two)
-  @optional_fields ~w()
-
   @doc """
   Creates a changeset based on the `model` and `params`.
 
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, @required_fields, @optional_fields)
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:player_one, :player_two])
+    |> validate_required([:player_one, :player_two])
   end
 
 end
