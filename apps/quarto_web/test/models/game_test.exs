e65fe4e8ee527eb9d180f8aec1559b8a8332d5d9
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-03 21:08:57 +0200

    adds a game test

--- apps/quarto_web/test/models/game_test.exs
+++ apps/quarto_web/test/models/game_test.exs
@@ -1,6 +1,19 @@
 defmodule QuartoWeb.GameTest do
   use QuartoWeb.ModelCase
 
-  alias QuartoWeb.Game
+  import QuartoWeb.Factory
 
+  alias QuartoWeb.{Game, User}
+
+  test "game with valid attributes" do
+    player_one = insert(:user)
+    player_two = insert(:user)
+
+    changeset = %Game{}
+      |> Game.changeset
+      |> Ecto.Changeset.put_assoc(:player_one, player_one)
+      |> Ecto.Changeset.put_assoc(:player_two, player_two)
+
+    assert {:ok, changeset} = Repo.insert changeset
+  end
 end
