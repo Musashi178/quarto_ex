1bb28ac23da59cd020cfcb596082b0e2be0d34f3
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-04 16:55:28 +0200

    fixes credo issue in dashboard_controller

--- apps/quarto_web/web/controllers/dashboard_controller.ex
+++ apps/quarto_web/web/controllers/dashboard_controller.ex
@@ -6,7 +6,7 @@ defmodule QuartoWeb.DashboardController do
   plug :scrub_params, "dashboard" when action in [:create, :update]
 
   def index(conn, %{"user_id" => user_id}) do
-    render(conn, "index.html" )
+    render(conn, "index.html")
   end
 
 end
