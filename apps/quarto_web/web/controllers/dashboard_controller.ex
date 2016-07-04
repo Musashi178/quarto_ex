d3b5d10e9b4abc0d4cb6d460256c789d7bfdc4ad
Marco Spatz <marco.spatz@kulzer-dental.com>
2016-07-04 22:43:47 +0200

    adds listing of user's games to the dashboard

--- apps/quarto_web/web/controllers/dashboard_controller.ex
+++ apps/quarto_web/web/controllers/dashboard_controller.ex
@@ -1,12 +1,15 @@
 defmodule QuartoWeb.DashboardController do
   use QuartoWeb.Web, :controller
 
-  alias QuartoWeb.{Dashboard, User}
+  alias QuartoWeb.{Dashboard, User, Game}
 
   plug :scrub_params, "dashboard" when action in [:create, :update]
 
   def index(conn, %{"user_id" => user_id}) do
-    render(conn, "index.html")
+    user = Repo.get!(User, user_id)
+    games = Game |> Game.for_user(user) |> Repo.all |> Repo.preload([:player_one, :player_two])
+
+    render(conn, "index.html", games: games)
   end
 
 end
--- apps/quarto_web/web/views/dashboard_view.ex
+++ apps/quarto_web/web/views/dashboard_view.ex
@@ -1,3 +1,10 @@
 defmodule QuartoWeb.DashboardView do
   use QuartoWeb.Web, :view
+
+  alias QuartoWeb.{User, Game}
+
+  def get_other_player(current_user, game) do
+      {:ok, other_player} = Game.get_other_player(game, current_user)
+      other_player
+  end
 end
